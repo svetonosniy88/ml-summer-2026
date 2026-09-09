9d0192ee7cc64da5b98d2d44e0863256b9ef5991
svetonosniy <ku2.dmitry@yandex.ru>
2026-09-09 09:08:29 +0300

    Update ML diagnostics and web chat context

--- diagnostics/iris_baseline/main.py
+++ diagnostics/iris_baseline/main.py
@@ -3,13 +3,5 @@ import matplotlib.pyplot as plt
 
 iris = load_iris(as_frame=True)
 
-plt.scatter(
-    iris.frame["petal length (cm)"],
-    iris.frame["petal width (cm)"],
-    c = iris.target,
-)
+print(iris.frame.head(10))
 
-plt.xlabel("Длина лепестка, см")
-plt.ylabel("Ширина лепестка, см")
-plt.title("Iris: лепестки и виды")
-plt.show()
